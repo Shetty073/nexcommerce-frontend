4e487772f9411257b15899d203978807ce5c8279
Ashish Shetty <shetty073@gmail.com>
2025-02-18 22:58:32 +0530

    margin fix

--- nexcommerce/src/pages/org/orders.page.jsx
+++ nexcommerce/src/pages/org/orders.page.jsx
@@ -1,7 +1,7 @@
 
 export default function OrdersPage() {
   return (
-    <div className="lg:ms-65 bg-white p-6 rounded-lg shadow-lg">
+    <div className="lg:ms-65 bg-white p-6 lg:mt-0 mt-22 rounded-lg shadow-lg">
       <h2 className="text-3xl font-bold text-indigo-800 mb-4">Orders Management</h2>
       <p className="text-gray-700">Here you can manage your Orders</p>
 
